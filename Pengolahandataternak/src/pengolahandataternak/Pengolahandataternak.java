576272abdc5751353dd4e6bfaa1456ed403704a4
nopalaswin <nopalaswin@gmail.com>
2024-11-18 21:37:45 +0800

    Add files via upload

---
+++ Pengolahandataternak/src/pengolahandataternak/Pengolahandataternak.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pengolahandataternak;
+
+import ConfigNpm.Config2210010151;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author ASUS
+ */
+public class Pengolahandataternak {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new Gui.Mainframe().setVisible(true);
+        // TODO code application logic here
+    }
+    
+}
